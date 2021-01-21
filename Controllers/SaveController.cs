8d8278c0569bd534bfb25787ff7cd8a61bba5d26
CoralieEspasa <coralie.espasa@gmail.com>
2021-01-21 16:15:02 +0100

    [-] #9: Delete update controller

--- Controllers/SaveController.cs
+++ Controllers/SaveController.cs
@@ -32,20 +32,25 @@ namespace Hospitalidée_CRM_Back_End.Controllers
             foreach(string siret in sirets)
             {
                 Etablissement selectedEtablissement = uniteLegale.etablissements.FirstOrDefault(etablissement => etablissement.siret == siret);
+                Etablissement existingEtablissement = _context.Etablissements.FirstOrDefault(e => e.siret == selectedEtablissement.siret);
                 if (selectedEtablissement != null)
                 {
-                    if (_context.Etablissements.Any(e => e.siret == selectedEtablissement.siret))
+                    if (existingEtablissement == selectedEtablissement)
                     {
-                        return BadRequest("Ce siret est déjà utilisé");
+                        etablissementBySiret.Remove(existingEtablissement);
+                        etablissementBySiret.Add(selectedEtablissement);
                     }
-
-                    etablissementBySiret.Add(selectedEtablissement);
+                    else
+                    {
+                        etablissementBySiret.Add(selectedEtablissement);
+                    }
                 }
 
             }
             uniteLegale.etablissements = etablissementBySiret;
             UniteLegale existingUniteLegale = _context.UniteLegale.Include(u=>u.etablissements).FirstOrDefault(u => u.siren == uniteLegale.siren);
-            if (existingUniteLegale != null)
+            
+            if (existingUniteLegale == uniteLegale)
             {
                 _context.Remove(existingUniteLegale);
                 _context.Add(uniteLegale);
@@ -54,6 +59,7 @@ namespace Hospitalidée_CRM_Back_End.Controllers
             {
                 _context.Add(uniteLegale);
             }
+            
             _context.SaveChanges();
             return Ok();
         }
